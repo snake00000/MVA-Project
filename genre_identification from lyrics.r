07355cb9eec14983a978b16da10a2984060f9831
rohanhore234509 <39278210+rohanhore234509@users.noreply.github.com>
2018-09-13 20:29:29 +0530

    Add files via upload

---
+++ genre_identification from lyrics.r
@@ -0,0 +1,96 @@
+
+##necessary packages
+library(tm)
+library(SnowballC)
+library(ggplot2)
+library(lsa)
+library(cluster)
+library(proxy)
+library(wordcloud)
+library(MASS)
+
+##data reading
+lyrics_data = read.csv("lyrics.csv",stringsAsFactors = F)
+lyrics_data=lyrics_data[sample(1:300000,1000,replace = F),]
+
+#str(text_data)
+colnames(lyrics_data)=c("doc_id",names(lyrics_data)[2:5],"text")
+#replacing \n with white spaces
+lyrics_data$text=gsub("[\r\n]", " ", lyrics_data$text)
+
+##making the corpus
+df_source=DataframeSource(lyrics_data)
+df_corpus = VCorpus(df_source)
+print(df_corpus)
+
+##pre-processing steps
+clean_corpus <- function(corpus) {
+  # Remove punctuation
+  corpus <- tm_map(corpus, removePunctuation)
+  #removing numbers
+  corpus <- tm_map(corpus, removeNumbers)
+  # Transform to lower case
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  # Add more stopwords
+  corpus <- tm_map(corpus, removeWords, c(stopwords("en")))
+  # Strip whitespace
+  corpus <- tm_map(corpus, stripWhitespace)
+  #stemming
+  corpus <- tm_map(corpus, stemDocument, language = "english")
+  return(corpus)
+}
+cleaned_corpus=clean_corpus(df_corpus)
+cleaned_corpus
+
+#save(cleaned_corpus,file = "lyrics_corpus.rds")
+#load("lyrics_corpus.rds")
+
+#pictorial visualisation of important words
+wordcloud(cleaned_corpus,max.words = 100)
+
+
+#Term document matrix based on Tf-idf weighting
+initial_td.mat<-TermDocumentMatrix(cleaned_corpus)
+initial_td.mat.tfidf<-as.matrix(weightTfIdf(initial_td.mat))
+
+##lsa
+lsa_space=lsa(initial_td.mat.tfidf,dims=100)
+td.mat.tfidf=lsa_space$tk%*%diag(lsa_space$sk)%*%t(lsa_space$dk)
+
+
+#cosine distance between feature vectors
+dist.mat.tfidf <- dist(t(as.matrix(td.mat.tfidf)),method = "cosine")
+dist.mat.tfidf  # check distance matrix
+
+#Multi-dimensional Scaling
+fit <- cmdscale(dist.mat.tfidf, eig = TRUE, k = 10)
+points <- data.frame(x = fit$points[, 1], y = fit$points[, 2])
+ggplot(points, aes(x = x, y = y),color=df$view) + geom_point(data = points, aes(x = x, y = y,color = df$view)) + geom_text(data = points, aes(x = x, y = y - 0.2, label = row.names(df)))
+
+#clustering(hierarchical) using tf_idf value
+groups <- hclust(dist.mat.tfidf,method="ward.D")
+plot(groups, cex=0.9, hang=-1)
+#visualisation of clusters
+rect.hclust(groups, k=13)
+
+
+#clustering (k means) using tf_idf values
+cluster<-kmeans(dist.mat.tfidf,10)
+cluster #inspection of cluster
+cluster$cluster
+table(cluster$cluster)
+
+#visualisation of clusters using first 2 principal components
+plot(prcomp(dist.mat.tfidf)$x, col=cluster$cluster)
+
+
+#elbow methhod for optimal k choice
+k.max <- 15
+wss <- sapply(1:k.max, 
+              function(k){kmeans(dist.mat.tfidf, k)$tot.withinss})
+wss
+plot(1:k.max, wss,
+     type="b", pch = 19, frame = FALSE, 
+     xlab="Number of clusters K",
+     ylab="Total within-clusters sum of squares")
+
